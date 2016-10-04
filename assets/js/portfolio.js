db12ff8b328cbbb5214b09a8569ab2427ce42500
Navin Udiaver <navinudiaver@Navins-MacBook-Pro.local>
2016-10-03 20:25:21 -0500

    initial commit

---
+++ assets/js/portfolio.js
@@ -0,0 +1,7 @@
+///The following function inserts the typed text into the introductory container. 
+  $(document).ready(function(){
+      $(".headertext").typed({
+        strings: ["<p>Navin Udiaver | Web Developer"],
+        typeSpeed: 30,
+      });
+  });
